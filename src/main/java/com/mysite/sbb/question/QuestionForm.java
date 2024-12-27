9b290ce5b1d44d05c136967a16f1d87ae28fb671
dana912 <iinnaa9911@gmail.com>
2024-12-27 13:21:29 +0900

    add modify/delete...

--- src/main/java/com/mysite/sbb/question/QuestionForm.java
+++ src/main/java/com/mysite/sbb/question/QuestionForm.java
@@ -2,6 +2,7 @@ package com.mysite.sbb.question;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
+
 import lombok.Getter;
 import lombok.Setter;
 
--- src/main/java/com/mysite/sbb/user/UserCreateForm.java
+++ src/main/java/com/mysite/sbb/user/UserCreateForm.java
@@ -3,6 +3,7 @@ package com.mysite.sbb.user;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
+
 import lombok.Getter;
 import lombok.Setter;
 
